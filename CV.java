d2aec8d0a3f6498151e47088c859d91c1a20e2d3
Nikitha <150829674+dnikitha1006@users.noreply.github.com>
2024-08-05 21:49:25 +0530

    Update CV.java

--- CV.java
+++ CV.java
@@ -51,3 +51,64 @@ img.setIcon(resize(location.getText()));
 }
 }
 });
+GENERATERESUMEButton.addActionListener(new ActionListener() {
+@Override
+public void actionPerformed(ActionEvent e) {
+if(name.getText()==null||contact.getText()==null||address.getText()==null||email.getText()==null){
+JOptionPane.showMessageDialog(null,"PLEASE ENTER ALL DETAILS TO GENERATE CV");
+}else{
+try {
+String nameOfFile = "D:\\PDFYAHA\\my.pdf";
+Document myDocument = new Document();
+PdfWriter.getInstance(myDocument, new FileOutputStream(nameOfFile));
+myDocument.open();
+com.itextpdf.text.Image img = com.itextpdf.text.Image.getInstance(location.getText());
+img.setAbsolutePosition(473f, 750f);
+img.scaleAbsolute(80f,70f);
+PdfPTable table = new PdfPTable(2);
+myDocument.add(img);
+myDocument.add(new Paragraph(name.getText(),FontFactory.getFont(FontFactory.TIMES_BOLD,32, com.itextpdf.text.Font.BOLD,BaseColor.DARK_GRAY )));
+myDocument.add(new Paragraph("",FontFactory.getFont(FontFactory.TIMES_BOLD,9,BaseColor.DARK_GRAY)));
+myDocument.add(new Paragraph("",FontFactory.getFont(FontFactory.TIMES_BOLD,9, BaseColor.DARK_GRAY)));
+myDocument.add(new Paragraph("----------------------------------------------------------------------------------------------------------------------------------"));
+myDocument.add(new Paragraph("CONTACT DETAILS",FontFactory.getFont(FontFactory.TIMES_BOLD,9, com.itextpdf.text.Font.BOLD,BaseColor.DARK_GRAY )));
+myDocument.add(new Paragraph(email.getText(),FontFactory.getFont(FontFactory.TIMES_BOLD,7,BaseColor.DARK_GRAY )));
+myDocument.add(new Paragraph(contact.getText(),FontFactory.getFont(FontFactory.TIMES_BOLD,7,BaseColor.DARK_GRAY )));
+myDocument.add(new Paragraph(address.getText(),FontFactory.getFont(FontFactory.TIMES_BOLD,7, BaseColor.DARK_GRAY )));
+myDocument.add(new Paragraph("----------------------------------------------------------------------------------------------------------------------------------"));
+myDocument.add(new Paragraph("SKILLS",FontFactory.getFont(FontFactory.TIMES_BOLD,9, com.itextpdf.text.Font.BOLD,BaseColor.DARK_GRAY )));
+table.setHeaderRows(1);
+table.addCell(skills1.getText());
+table.addCell(skills2.getText());
+table.addCell(skills3.getText());
+table.addCell(skills4.getText());
+myDocument.add(table);
+myDocument.add(new Paragraph("----------------------------------------------------------------------------------------------------------------------------------"));
+myDocument.add(new Paragraph("QUALIFICATIONS", FontFactory.getFont(FontFactory.TIMES_BOLD,9, com.itextpdf.text.Font.BOLD, BaseColor.DARK_GRAY )));
+myDocument.add(new Paragraph(college.getText(),FontFactory.getFont(FontFactory.TIMES_BOLD,7,BaseColor.DARK_GRAY )));
+myDocument.add(new Paragraph(qualiA.getText(),FontFactory.getFont(FontFactory.TIMES_BOLD,7,BaseColor.DARK_GRAY )));
+myDocument.add(new Paragraph(qualiB.getText(),FontFactory.getFont(FontFactory.TIMES_BOLD,7,BaseColor.DARK_GRAY )));
+myDocument.add(new Paragraph("----------------------------------------------------------------------------------------------------------------------------------"));
+myDocument.add(new Paragraph("WORK EXPERIENCE",FontFactory.getFont(FontFactory.TIMES_BOLD,10, com.itextpdf.text.Font.BOLD,BaseColor.DARK_GRAY )));
+myDocument.add(new Paragraph(""+work.getSelectedItem(),FontFactory.getFont(FontFactory.TIMES_BOLD,7,BaseColor.DARK_GRAY)));
+myDocument.add(new Paragraph("----------------------------------------------------------------------------------------------------------------------------------"));
+myDocument.add(new Paragraph("REFERENCES",FontFactory.getFont(FontFactory.TIMES_BOLD,9, com.itextpdf.text.Font.BOLD,BaseColor.DARK_GRAY )));
+myDocument.add(new Paragraph("Available upon request",FontFactory.getFont(FontFactory.TIMES_BOLD,6,BaseColor.DARK_GRAY )));
+myDocument.close();
+JOptionPane.showMessageDialog(null,"CV was successfully generated");
+}
+catch(Exception ex){
+JOptionPane.showMessageDialog(null,ex);
+}
+}
+}
+});
+}
+public ImageIcon resize(String path){
+ImageIcon myImg = new ImageIcon(path);
+Image image = myImg.getImage();
+Image newImage = image.getScaledInstance(200,200,Image.SCALE_SMOOTH);
+ImageIcon finalImage = new ImageIcon(newImage);
+return finalImage;
+}
+}
